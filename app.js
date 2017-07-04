3477d0b28db4da65f873337698c0a85c08471413
Mathias San Miguel <mathiassanmiguel@gmail.com>
2017-07-03 21:39:52 -0400

    Fixed deployment

--- app.js
+++ app.js
@@ -2,12 +2,29 @@
 
 const path = require('path');
 
-const compression = require('compression')
-const express = require('express');
+const express       = require('express');
+const compression   = require('compression')
+//const serveStatic   = require('serve-static');
+
+const port    = process.env.PORT || 3000;
+const domain  =  process.env.DOMAIN;
+
 const app = express();
 const router = express.Router();
 
 const raiz = path.join(__dirname, 'app');
+
+function ensureDomain(req, res, next) {
+  if (!domain || req.hostname === domain) {
+    // OK, continue
+    return next();
+  };
+
+  // handle port numbers if you need non defaults
+  res.redirect(`http://${domain}${req.url}`);
+};
+
+app.all('*', ensureDomain);
 
 app.use(compression());
 app.use(express.static( '.tmp' ) );
@@ -16,4 +33,6 @@ app.use('/scripts', express.static( path.join(raiz,'scripts') ) );
 app.use('/styles',  express.static( path.join(raiz,'styles') ) );
 app.get('/', (req, res) => res.sendFile( path.join(raiz,'index.html') ) );
  
-app.listen(5000); 
+app.listen(port, () => {
+  console.log('Server running...');
+});
